36f2af20e927e831bee3f24f75936d63832622d4
purpleplusapple <purpleplusapple@gmail.com>
2026-01-29 23:06:07 +0700

    done

--- 38_Balcony_Terrace_AI/lib/src/app_assets.dart
+++
@@ -1,9 +0,0 @@
-// lib/src/app_assets.dart
-// Asset paths for Balcony Terrace AI
-
-class AppAssets {
-  AppAssets._();
-
-  /// App icon asset
-  static const String appIcon = 'assets/icon.jpg';
-}
